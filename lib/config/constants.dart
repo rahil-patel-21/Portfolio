15ded3c8a7ee9e15f9fc599eb21c118a9002b221
Rahil Patel <rahil.geek21@outlook.com>
2020-07-14 07:35:26 +0530

    first commit

---
+++ lib/config/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const BLOG_API =
+      'https://adityadroid-medium.herokuapp.com/@adityadroid';
+  static const MEDIUM_IMAGE_CDN = 'https://miro.medium.com/fit/c/700/300/';
+  static const BLOG_URL = 'http://www.medium.com/@adityadroid/';
+  static const PROFILE_MEDIUM = 'http://www.medium.com/@adityadroid';
+  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/rahil-rp21/';
+  static const PROFILE_FACEBOOK = 'https://m.me/rahil.rp21';
+  static const PROFILE_GITHUB = 'https://github.com/rahil-patel-21';
+  static const PROFILE_TWITTER = 'https://twitter.com/rahil_rp21';
+  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/rahil.geek21/';
+}
---
+++ lib/config/projects.dart
@@ -0,0 +1,60 @@
+import '../models/project_model.dart';
+import 'assets.dart';
+
+final List<Project> projects = [
+  Project(
+      name: 'Foodie Woodie',
+      image: Assets.truelancer,
+      description: 'One Common Place for All Foodies.',
+      link: 'https://play.google.com/store/apps/details?id=com.truelancer.app'),
+  Project(
+      name: 'Mate Communities',
+      image: Assets.messio,
+      description: 'A Great Place to for Roommate, Classmate, Etc. Seekers',
+      link: 'https://github.com/adityadroid/messio'),
+  Project(
+      name: 'Savaari',
+      image: Assets.savaari_consumer,
+      description:
+          'Savaari is the leading player in Outstation Cabs, Hourly Rental Cabs, Airport Pickups and Airport Drop Taxis. ',
+      link: 'https://play.google.com/store/apps/details?id=com.savaari.app'),
+  Project(
+      name: 'Savaari Partner',
+      image: Assets.savaari_partner,
+      description:
+          'This App allows cab drivers to conveniently share billing and other trip details scheduled to them.',
+      link:
+          'https://play.google.com/store/apps/details?id=com.savaari.driver.app'),
+  Project(
+      name: 'Mynewcar',
+      image: Assets.mynewcar,
+      description:
+          'Through this App, along with doorstep delivery of a new car, one can also select the accessories to buy.',
+      link: 'https://play.google.com/store/apps/details?id=com.mynewcar.app'),
+  Project(
+      name: 'Trivz',
+      image: Assets.trivz,
+      description:
+          'With the Trivz app, car owners can give test drives to car buyers.',
+      link:
+          'https://play.google.com/store/apps/details?id=in.mynewcar.testdrives'),
+  Project(
+      name: 'Mydealer',
+      image: Assets.mydealer,
+      description:
+          'Mydealer App helps a car dealership to manage sales process and improve the productivity`.',
+      link: 'https://play.google.com/store/apps/details?id=in.mnc.mydealer'),
+  Project(
+      name: 'FaceLyt',
+      image: Assets.facelyt,
+      description:
+          'FaceLyt is an alternative and free client to manage your Facebook account.',
+      link:
+          'https://apkpure.com/facelyt-for-facebook-lite/com.adityaadi1467.facelytx'),
+  Project(
+      name: 'Cocoapay',
+      image: Assets.cocoapay,
+      description:
+          'Cocoapay helps merchants start accepting payments through POS devices and bill through the app.',
+      link: 'http://www.cocoapay.com/'),
+];
